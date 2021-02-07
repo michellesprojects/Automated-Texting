79b68f3f54db4aa7365da5f0b6e96d90f896e335
michellesprojects <69691406+michellesprojects@users.noreply.github.com>
2021-02-07 14:09:29 -0500

    Add comments

--- swbot.py
+++ swbot.py
@@ -1,16 +1,15 @@
 import pyautogui
 
-#move mouse to the x & y coordinates of where the message text box is
+#move mouse to the x & y coordinates of where the text box is to type your message
 pyautogui.moveTo(83,799,1)
 
-pyautogui.click()
+pyautogui.click() #this code is optimized for the desktop discord application, this lines clicks onto the text box for messages
 
+file = open('swscript.txt','r') #file with swscript
 
-file = open('swscript.txt','r')
-
-for line in file:
-	line = line.strip()
-	if len(line) > 0:
-		pyautogui.write(line)
-		pyautogui.press('enter')
+for line in file: #iterating through all lines in the script
+	line = line.strip() 
+	if len(line) > 0: #ignoring lines with 0 chars
+		pyautogui.write(line) #write line into text box
+		pyautogui.press('enter') #press enter to send messsage
 
